59cace4df002345496bafbcea292f149eaa16412
Pierre Lenertz <pierre@lenertz.fr>
2022-09-16 14:13:39 +0200

    Adding a redeploy script

---
+++ redeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd SCRIPTPATH
+sudo systemctl stop coconutbot.service
+git pull
+npm install
+npx tsc
+sudo systemctl start coconutbot.service
+
+exit 0;
