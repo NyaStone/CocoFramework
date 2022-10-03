75d2061e9c94ed70bd5b224e62ea261ab0cc6cd3
Pierre Lenertz <pierre@lenertz.fr>
2022-10-03 22:12:59 +0200

    Polishing the interact command

--- discord/commands/apps/Interact.command.ts
+++ discord/commands/apps/Interact.command.ts
@@ -1,3 +1,4 @@
+import { User } from "discord.js";
 import { LastGif } from "../../../assets/LastGif.class";
 import { RandomImageAPI } from "../../../web/routers/RandomImageAPI.router";
 import { UsageError } from "../../errors/UsageError.class";
@@ -9,8 +10,8 @@ import { StringArgument } from "../arguments/StringArgument.class";
 import { UserArgument } from "../arguments/UserArgument.class";
 
 
-export class Interract extends AbstractCommand {
-    static commandName: string = 'i';
+export class Interact extends AbstractCommand {
+    static commandName: string = 'interact';
     static description: string = 'Command used to interract with a user using an anime gif.';
     static commandArguments: AbstractArgument[] = [
         new StringArgument('action', 'How you want to interract with the user', undefined, true, [
@@ -24,13 +25,42 @@ export class Interract extends AbstractCommand {
             new ArgumentChoice<string>('vibe', 'vibe'),
             new ArgumentChoice<string>('yeet', 'yeet')
         ]),
-        new UserArgument('target', 'Whom you want to interract with.', false)
+        new UserArgument('target', 'Whom you want to interract with.', true),
+        new StringArgument('flavor', 'Flavor text you want to add with the interraction.', false),
+        new UserArgument('target2', 'Another person you might wanna interract with.', false),
+        new UserArgument('target3', 'Another person you might wanna interract with.', false),
+        new UserArgument('target4', 'Another person you might wanna interract with.', false),
+        new UserArgument('target5', 'Another person you might wanna interract with.', false),
+        new UserArgument('target6', 'Another person you might wanna interract with.', false),
+        new UserArgument('target7', 'Another person you might wanna interract with.', false),
+        new UserArgument('target8', 'Another person you might wanna interract with.', false),
+        new UserArgument('target9', 'Another person you might wanna interract with.', false),
+        new UserArgument('target10', 'Another person you might wanna interract with.', false)
     ];
+
+
+    /**
+     * Method to get the list of targets of this command
+     * @returns An array of user in order
+     */
+    private getTargets(): User[] {
+        const res: User[] = [];
+        // getting the primary target which is a required option
+        const firstTarget = this.getUserArgument('target');
+        if (!firstTarget) throw new UsageError('Missing the first target which is required option');
+        res.push(firstTarget);
+        // looping to get the optional targets
+        for (let i = 2; i <= 10; i++) {
+            const target = this.getUserArgument(`target${i}`);
+            if (target) res.push(target);
+        }
+        return res;
+    }
 
     async run() {
         // getting the arguments
         const action = this.getStringArgument('action');
-        const target = this.getUserArgument('target');
+        const flavor = this.getStringArgument('flavor');
         // security check
         if (!action) throw new UsageError('Missing an action for the interaaction command');
         // looking up the database to avoid getting the same gif twice
@@ -39,7 +69,7 @@ export class Interract extends AbstractCommand {
         const randomImageAPIResponse = await RandomImageAPI.fetch(action, lastGif.name);
         lastGif.name = randomImageAPIResponse.name;
         const imageURL: string = randomImageAPIResponse.url;
-        const response: InteractionView = new InteractionView(action, imageURL, this.interaction.user, target); 
+        const response: InteractionView = new InteractionView(action, imageURL, this.interaction.user, this.getTargets(), flavor); 
         await this.reply(response);
         return this;
     }
--- discord/views/InteractionView.class.ts
+++ discord/views/InteractionView.class.ts
@@ -1,14 +1,53 @@
 import { User } from "discord.js";
+import { UsageError } from "../errors/UsageError.class";
 import { AbstractSuccessView } from "./AbstractSuccessView.class";
 
+type verb = {singular: string, plural: string};
+
 export class InteractionView extends AbstractSuccessView {
-    constructor(action: string, url: string, user: User, target?: User) {
+    static verbs: Map<string, verb> = new Map<string, verb>([
+        ['bonk', {singular: 'is getting bonked', plural: 'are getting bonked'}],
+        ['hug', {singular: 'is getting hugged', plural: 'are getting hugged'}],
+        ['kiss', {singular: 'is getting kissed', plural: 'are getting kissed'}],
+        ['lick', {singular: 'is getting licked', plural: 'are getting licked'}],
+        ['pat', {singular: 'is getting patted', plural: 'are getting patted'}],
+        ['sleep', {singular: 'is being told to go to sleep', plural: 'are being told to go to sleep'}],
+        ['stare', {singular: 'is gettng stared at', plural: 'are getting stared at'}],
+        ['vibe', {singular: 'is being told it\'s time to dance', plural: 'are being told it\'s time to dance'}],
+        ['hug', {singular: 'is getting yeeted away', plural: 'are getting yeeted away'}]
+    ]);
+
+    constructor(action: string, url: string, user: User, targets: User[], flavorText?: string) {
         super();
+        // setting the author of the command
         const authorURL = user.avatarURL();
         if (!authorURL) throw new Error('why no user url?');
         this.setAuthor({ name: user.username, iconURL: authorURL});
-        if (target) this.setDescription(`${action} ${target.toString()}`);
-        console.log(url);
+        // building the string pinging all the targets and remembering if it's one or mutiple
+        let pingString: string = targets[0].toString();
+        let singularInteraction: boolean = true;
+        if (targets.length > 1) {
+            singularInteraction = false;
+            for (let i = 1; i < targets.length - 1; i++) {
+                pingString += `, ${targets[i].toString()}`;
+            }
+            pingString += ` and ${targets[targets.length - 1]}`;
+        }
+        // getting the action string
+        let actionString: string;
+        const verb = InteractionView.verbs.get(action);
+        if (!verb) throw new UsageError('The action has not been found');
+        if (singularInteraction) {
+            actionString = verb.singular;
+        }
+        else {
+            actionString = verb.plural;
+        }
+        // building the embed description
+        let description: string = `${pingString} ${actionString}.`;
+        if (flavorText) description += `\n${flavorText}`;
+        // finishing the embed build
+        this.setDescription(description);
         this.setImage(url);
     }
 }
