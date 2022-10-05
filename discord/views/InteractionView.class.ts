2907387a5e3075354abbbdf496ab59d75cf32af7
Pierre Lenertz <pierre@lenertz.fr>
2022-10-05 20:37:53 +0200

    reworking the view to display the counters

--- discord/views/InteractionView.class.ts
+++ discord/views/InteractionView.class.ts
@@ -1,24 +1,33 @@
 import { User } from "discord.js";
+import { InteractionCounter } from "../../assets/interactions/InteractionCounter.class";
 import { UsageError } from "../errors/UsageError.class";
 import { AbstractSuccessView } from "./AbstractSuccessView.class";
 
-type verb = {singular: string, plural: string};
+type act = {footer: string, desc: string};
 
 export class InteractionView extends AbstractSuccessView {
-    static verbs: Map<string, verb> = new Map<string, verb>([
-        ['bonk', {singular: 'is getting bonked', plural: 'are getting bonked'}],
-        ['hug', {singular: 'is getting hugged', plural: 'are getting hugged'}],
-        ['kiss', {singular: 'is getting kissed', plural: 'are getting kissed'}],
-        ['lick', {singular: 'is getting licked', plural: 'are getting licked'}],
-        ['pat', {singular: 'is getting patted', plural: 'are getting patted'}],
-        ['sleep', {singular: 'is being told to go to sleep', plural: 'are being told to go to sleep'}],
-        ['stare', {singular: 'is gettng stared at', plural: 'are getting stared at'}],
-        ['vibe', {singular: 'is being told it\'s time to dance', plural: 'are being told it\'s time to dance'}],
-        ['yeet', {singular: 'is getting yeeted away', plural: 'are getting yeeted away'}]
+    static verbs: Map<string, act> = new Map<string, act>([
+        ['bonk', {footer: 'bonked them', desc: 'getting bonked'}],
+        ['hug', {footer: 'hugged them', desc: 'getting hugged'}],
+        ['kiss', {footer: 'kissed them', desc: 'getting kissed'}],
+        ['lick', {footer: 'licked them', desc: 'getting licked'}],
+        ['pat', {footer: 'patted them', desc: 'getting patted'}],
+        ['sleep', {footer: 'sent them to sleep', desc: 'being told to go to sleep'}],
+        ['stare', {footer: 'stared at them', desc: 'gettng stared at'}],
+        ['vibe', {footer: 'danced with them', desc: 'being told it\'s time to dance'}],
+        ['yeet', {footer: 'yeeted them away', desc: 'getting yeeted away'}]
     ]);
+
+    private user: User;
+    private targets: User[];
+    private act: act;
+    private action: string;
 
     constructor(action: string, url: string, user: User, targets: User[], flavorText?: string) {
         super();
+        this.user = user;
+        this.targets = targets;
+        this.action = action;
         // setting the author of the command
         const authorURL = user.avatarURL();
         if (!authorURL) throw new Error('why no user url?');
@@ -33,15 +42,17 @@ export class InteractionView extends AbstractSuccessView {
             }
             pingString += ` and ${targets[targets.length - 1]}`;
         }
+        // getting the action object
+        const act = InteractionView.verbs.get(action);
+        if (!act) throw new UsageError('The action has not been found');
+        this.act = act;
         // getting the action string
         let actionString: string;
-        const verb = InteractionView.verbs.get(action);
-        if (!verb) throw new UsageError('The action has not been found');
         if (singularInteraction) {
-            actionString = verb.singular;
+            actionString = `is ${this.act.desc}`;
         }
         else {
-            actionString = verb.plural;
+            actionString = `are ${this.act.desc}`;
         }
         // building the embed description
         let description: string = `${pingString} ${actionString}.`;
@@ -49,5 +60,23 @@ export class InteractionView extends AbstractSuccessView {
         // finishing the embed build
         this.setDescription(description);
         this.setImage(url);
+    }
+
+    async generateFooter() {
+        // get the first targets count
+        const firstTargetCounter: InteractionCounter = await InteractionCounter.construct(this.action, this.user.id, this.targets[0].id);
+        let countString: string = `${firstTargetCounter.count}`;
+        // if there are more targets, looping on the array 
+        if (this.targets.length > 1) {
+            for (let i = 1; i < this.targets.length - 1; i++) {
+                const curTargetCounter: InteractionCounter = await InteractionCounter.construct(this.action, this.user.id, this.targets[i].id);
+                countString += `, ${curTargetCounter.count}`;
+            }
+            // for the last one, use "and" istead of a ","
+            const lastTargetCounter: InteractionCounter = await InteractionCounter.construct(this.action, this.user.id, this.targets[this.targets.length - 1].id);
+            countString += ` and ${lastTargetCounter.count}`;
+        }
+        this.setFooter({text: `You have ${this.act.footer} ${countString} times`});
+        return;
     }
 }
